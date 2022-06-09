927596fea0d3d447c0bc1a5cbd4cb6a9f88347bf
John Balderson <38844243+johnbalderson@users.noreply.github.com>
2022-06-09 03:46:15 -0400

    Add files via upload
    Initial commit of final project

---
+++ predict.py
@@ -0,0 +1,114 @@
+import argparse
+import numpy as np
+import json
+
+import torch
+from torchvision import datasets, transforms, models
+
+from image_utility import process_image
+from model import load_checkpoint
+
+#  Predict flower name from an image with predict.py along with the probability of that name.
+#  That is, you'll pass in a single image /path/to/image and return the flower name and class probability.
+
+#  Basic usage: python predict.py /path/to/image checkpoint
+#  Options:
+#  Return top KK most likely classes: python predict.py input checkpoint --top_k 3
+#  Use a mapping of categories to real names: python predict.py input checkpoint --category_names cat_to_name.json
+#  Use GPU for inference: python predict.py input checkpoint --gpu
+
+#  Argumwnts:
+#
+#  image_dir      - path to image in directory where images are stored (default in flowers directory)
+#  model_input    - location of path of model checkpoint (default is checkpoint.pth)
+#  top_k          - set number of top k most likely classes (default is 5)
+#  category_names - set file for mapping of flower categories to category names (default is cat_to_name.json)
+#  device         - set device mode cuda or cpu (defailt is cuda)
+
+parser = argparse.ArgumentParser(description='Predict the top K most likely flower classes based on image path and saved checkpoint')
+
+# Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
+parser.add_argument('--image_dir', type=str, default='./flowers/test/44/image_01516.jpg',
+                        help='set path of the flower image (default=./flowers/test/44/image_01516.jpg)')
+
+parser.add_argument('--model_input', type=str, default='checkpoint.pth',
+                        help='set path of the model checkpoint (default=checkpoint.pth)')
+
+parser.add_argument('--top_k', type=int, default=5,
+                        help='set number of top K most likely classes (default=5)')
+
+parser.add_argument('--category_names', type=str, default='cat_to_name.json',
+                        help='set file for mapping of flower categories to category names (default=cat_to_name.json)')
+
+parser.add_argument('--device', type=str, default='cuda', choices=['cuda', 'cpu'],
+                        help='set device mode cuda or cpu (default=cuda)')
+
+results = parser.parse_args()
+
+image_dir = results.image_dir
+model_input = results.model_input
+top_k = results.top_k
+
+# check to see if cuda is available otherwise use cpu
+
+if results.device == 'cuda' and torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('CUDA GPU mode is enabled now')
+elif results.device == 'cpu':
+    device = torch.device('cpu')
+    print('CPU mode is enabled now')
+else:
+    print('CUDA GPU is not supported on this system so switching to CPU mode')
+    device = torch.device('cpu')
+
+category_names = results.category_names
+with open(category_names, 'r') as f:
+    cat_to_name = json.load(f)
+
+model, optimizer = load_checkpoint(model_input)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+
+# Class Prediction (The predict function successfully takes the path to an image and a checkpoint,
+# then returns the top K most probably classes for that image)
+# Predicting with GPU: The function allows users to use the GPU or CPU to calculate the predictions
+def predict(image_dir, model, topk, device):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+
+    # TODO  Implement the code to predict the class from an image file
+    model.to(device)
+    model.eval()
+
+    image_tensor = process_image(image_dir)
+    image_tensor = image_tensor.to(device)
+
+    with torch.no_grad():
+        output = model.forward(image_tensor)
+
+    ps = torch.exp(output)
+
+    # Top K classes: the function predicts the top K classes along with associated probabilities
+    probs_topk = np.array(ps.topk(topk)[0])[0]
+    class_idx = np.array(ps.topk(topk)[1])[0]
+
+    # Invert the model.class_to_idx dict to obtain mapping
+    idx_to_class = dict((value, key) for key, value in model.class_to_idx.items())
+    class_topk = []
+    for idx in class_idx:
+        class_topk.append(idx_to_class[idx])
+
+    return image_tensor, probs_topk, class_topk
+
+
+# Predicting classes: The predict function successfully reads in an image and a checkpoint
+# then returns the most likely image class index and its associated probability
+# Run prediction function
+image_tensor, probs_topk, class_topk = predict(image_dir, model, top_k, device)
+
+predicted_flower_names = [cat_to_name[idx] for idx in class_topk]
+print("{} most likely predicted flower names = {}".format(top_k, predicted_flower_names))
+print("{} most likely predicted probability = {}".format(top_k, probs_topk))
+print("The model predicted flower name as {} with {:.3f}% probability".format(predicted_flower_names[0], 100 * probs_topk[0]))
---
+++ train.py
@@ -0,0 +1,138 @@
+import argparse
+import json
+
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+from collections import OrderedDict
+
+from image_utility import load_transform_data
+from model import save_checkpoint, train_model, test_model
+
+# Train a new network on a data set with train.py
+
+# Basic usage: python train.py data_directory
+# Prints out training loss, validation loss, and validation accuracy as the network trains
+#
+# Options:
+# Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
+# Choose architecture: python train.py data_dir --arch "vgg13"
+# Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
+#Use GPU for training: python train.py data_dir --gpu
+#
+# Arguments:
+#
+# data-dir         - Path of training image folder (defaukt is 'flowers')
+# save-dir         - Path to save model checkpoint (default is checkpoint.pth)
+# arch             - select vgg16 or densenet121 model architecture (default is vgg16)
+# category_names   - fle for mapping flower categories to category names (default is cat_to_name.json)
+# learning_rate    - set learning rate (defailt is 0.003)
+# hidden_units     - set number of hidden units (default is 500)
+# dropout          - set dropout rate (default is 0.03)
+# epochs           - set number of epochs (default is 3)
+# device           - set device type mode for cuda or cpu (default is cuda)
+
+
+parser = argparse.ArgumentParser(description='Train a neural network on a dataset of images and save the model checkpoint')
+
+# Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
+parser.add_argument('--data_dir', type=str, default='flowers',
+                        help='set path of the training image folder (default=flowers)')
+
+parser.add_argument('--save_dir', type=str, default='checkpoint.pth',
+                        help='set path of the model checkpoint (default=checkpoint.pth)')
+
+parser.add_argument('--arch', type=str, default='vgg16', choices=['vgg16', 'densenet121'],
+                    help='Choose cnn model architecture either vgg16 or densenet121 (default=vgg16')
+
+parser.add_argument('--category_names', type=str, default='cat_to_name.json',
+                        help='set file for mapping of flower categories to category names (default=cat_to_name.json)')
+
+parser.add_argument('--learning_rate', type=float, default=0.003,
+                        help='set learning rate (default=0.003')
+
+parser.add_argument('--hidden_units', type=int, default=500,
+                        help='set number of hidden units (default=500')
+
+parser.add_argument('--dropout', type=float, default=0.03,
+                        help='set dropout rate (default=0.03')
+
+parser.add_argument('--epochs', type=int, default=3,
+                        help='set number of epochs (default=3')
+
+parser.add_argument('--device', type=str, default='cuda', choices=['cuda', 'cpu'],
+                        help='set device mode cuda or cpu (default=cuda)')
+
+# Retrieving command line argument options
+results = parser.parse_args()
+
+data_dir = results.data_dir
+save_dir = results.save_dir
+
+learning_rate = results.learning_rate
+hidden_units = results.hidden_units
+drop_out_probability = results.dropout
+num_epochs = results.epochs
+
+if results.device == 'cuda' and torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('CUDA GPU mode is enabled now')
+elif results.device == 'cpu':
+    device = torch.device('cpu')
+    print('CPU mode is enabled now')
+else:
+    print('CUDA GPU is not supported on this system so switching to CPU mode')
+    device = torch.device('cpu')
+
+category_names = results.category_names
+with open(category_names, 'r') as f:
+    cat_to_name = json.load(f)
+
+train_data, test_data, valid_data, trainloader, testloader, validloader = load_transform_data(data_dir)
+
+# Calculate the label count
+output_layer_label_count = len(train_data.class_to_idx)
+
+# Model architecture: The training script allows users to choose from
+# least two different architectures available from torchvision.models
+# Model hyperparameters: The training script allows users to set hyperparameters for learning rate, number of hidden units
+# A new feedforward network is defined for use as a classifier using the features as input
+arch = results.arch
+if arch == 'vgg16':
+    model = models.vgg16(pretrained=True)
+    classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(25088, hidden_units)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(drop_out_probability)),
+                          ('fc2', nn.Linear(hidden_units, output_layer_label_count)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+elif arch == 'densenet121':
+    model = models.densenet121(pretrained=True)
+    classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(1024, hidden_units)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(drop_out_probability)),
+                          ('fc2', nn.Linear(hidden_units, output_layer_label_count)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+
+# Freeze parameters so we don't backprop through them
+for param in model.parameters():
+    param.requires_grad = False
+
+model.classifier = classifier
+
+# Define the loss
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+# Optimizers require the parameters to optimize and a learning rate
+optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+
+model, optimizer = train_model(model, trainloader, validloader, criterion, num_epochs, optimizer, device)
+test_model(model, testloader, device)
+
+save_checkpoint(model, optimizer, train_data, arch, save_dir)
